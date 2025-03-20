58532af5b38ceb0729ca5a15183ec592d7f6d939
David Llewellyn-Jones <david@flypig.co.uk>
2025-03-20 00:52:03 +0000

    Add support for downloading media streams
    Adds methods for downloading media streams, plus some example C code to
    demonstrate its use.

--- cpp/main.c
+++ cpp/main.c
@@ -5,7 +5,41 @@
 
 #include "appwrapper.h"
 
-int main(int argc, char **argv) {
+void printExtracted(char* response) {
+    Json *json;
+    json = json_new();
+
+    bool success = json_deserialize_string(json, response, strlen(response));
+    if (success) {
+        char const* string;
+        long long int number;
+
+        string = json_get_string(json, "name");
+        printf("Name: %s\n", string);
+
+        string = json_get_string(json, "uploaderName");
+        printf("Uploader name: %s\n", string);
+
+        string = json_get_string(json, "category");
+        printf("Category: %s\n", string);
+
+        number = json_get_integer(json, "likeCount");
+        printf("Like count: %lld\n", number);
+
+        number = json_get_integer(json, "viewCount");
+        printf("View count: %lld\n", number);
+
+        string = json_get_string(json, "content");
+        printf("Content: %s\n", string);
+    }
+    else {
+        printf("Json deserialization failed: %s\n", response);
+    }
+
+    json_delete(json);
+}
+
+int main(int argc, char** argv) {
     graal_isolate_t *isolate = NULL;
     graal_isolatethread_t *thread = NULL;
     
@@ -18,44 +52,33 @@ int main(int argc, char **argv) {
     
     Buffer *buffer = buffer_new(1024);
     Json *json;
+    char* result;
 
     json = json_new();
 
     // First call
-    json_add_string(json, "name", "World");
+    json_add_string(json, "service", "YouTube");
+    json_add_string(json, "url", "https://www.youtube.com/watch?v=NWqC9cORKi0");
     json_serialize_buffer(json, buffer);
 
-    char * result = invoke(thread, "helloWorld", buffer_get_buffer(buffer));
+    result = invoke(thread, "downloadExtract", buffer_get_buffer(buffer));
+    printExtracted(result);
 
-    bool success = json_deserialize_string(json, result, strlen(result));
-    if (success) {
-        char const * name = json_get_string(json, "result");
-        printf("Result: %s\n", name);
-    }
-    else {
-        printf("Json deserialization filed: %s\n", result);
-    }
-
-    // Second call
     json_delete(json);
     json = json_new();
 
-    json_add_string(json, "result", "World");
+    // Second call
+    json_add_string(json, "service", "SoundCloud");
+    json_add_string(json, "url", "https://soundcloud.com/highviberecordsca/matt-williams-sailfish");
     json_serialize_buffer(json, buffer);
 
-    result = invoke(thread, "worldHello", buffer_get_buffer(buffer));
-
-    success = json_deserialize_string(json, result, strlen(result));
-    if (success) {
-        char const * name = json_get_string(json, "name");
-        printf("Result: %s\n", name);
-    }
-    else {
-        printf("Json deserialization filed: %s\n", result);
-    }
+    result = invoke(thread, "downloadExtract", buffer_get_buffer(buffer));
+    printExtracted(result);
 
     json_delete(json);
     buffer_delete(buffer);    
+
+    result = invoke(thread, "tearDown", "{}");
 
     graal_tear_down_isolate(thread);
     return 0;
