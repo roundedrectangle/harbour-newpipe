b903a3882c58bbdc4de6fdafd42fc4b30c5f0e6c
legacychimera247 <mad_fer_it@vivaldi.net>
2025-07-18 20:01:26 +0100

    Add Italian translations
    Adds Italian translations to the app, generously provided by
    legacychimera247.

---
+++ sfos/harbour-newpipe/translations/harbour-newpipe-it.ts
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name></name>
+    <message id="newpipe-comment_item-replies" numerus="yes">
+        <source>%n replies</source>
+        <translation>
+            <numerusform>%n risposta</numerusform>
+            <numerusform>%n risposte</numerusform>
+        </translation>
+    </message>
+    <message id="newpipe-replies_page-header">
+        <source>Replies</source>
+        <translatorcomment>Risposte</translatorcomment>
+        <translation>Replies</translation>
+    </message>
+    <message id="newpipe-proglist_search-placeholder">
+        <source>Search</source>
+        <translation>Ricerca</translation>
+    </message>
+    <message id="newpipe-proglist_search-no-entries">
+        <source>No entries</source>
+        <translation>Nessuna ricerca</translation>
+    </message>
+    <message id="newpipe-proglist_search-enter-some-text">
+        <source>Enter some text to search</source>
+        <translation>Inserisci del testo per avviare la ricerca</translation>
+    </message>
+    <message id="newpipe-video_page-header">
+        <source>YouTube Video</source>
+        <translation>Video YouTube</translation>
+    </message>
+    <message id="newpipe-cover-title">
+        <source>NewPipe</source>
+        <translation>NewPipe</translation>
+    </message>
+    <message id="newpipe-media_details-detail_uploader">
+        <source>Uploader</source>
+        <translation>Caricato da</translation>
+    </message>
+    <message id="newpipe-media_details-detail_category">
+        <source>Category</source>
+        <translation>Categoria</translation>
+    </message>
+    <message id="newpipe-media_details-detail_description">
+        <source>Description</source>
+        <translation>Descrizione</translation>
+    </message>
+    <message id="newpipe-media_details-detail_date">
+        <source>Upload date</source>
+        <translation>Data caricamento</translation>
+    </message>
+    <message id="newpipe-media_details-detail_length">
+        <source>Length</source>
+        <translation>Lunghezza</translation>
+    </message>
+    <message id="newpipe-media_details-detail_licence">
+        <source>Licence</source>
+        <translation>Licenza</translation>
+    </message>
+    <message id="newpipe-searchitem-stream_inforow">
+        <source>%0 • %1</source>
+        <translation>%0 • %1</translation>
+    </message>
+    <message id="newpipe-searchitem-channel_stream_count" numerus="yes">
+        <source>%n items</source>
+        <oldsource>%n item(s)</oldsource>
+        <translation>
+            <numerusform>%n oggetto</numerusform>
+            <numerusform>%n oggetti</numerusform>
+        </translation>
+    </message>
+    <message id="newpipe-searchitem-channel_subscriber_count" numerus="yes">
+        <source>%n subscribers</source>
+        <oldsource>%n subscriber(s)</oldsource>
+        <translation>
+            <numerusform>%n sottoscritto</numerusform>
+            <numerusform>%n sottoscritti</numerusform>
+        </translation>
+    </message>
+    <message id="newpipe-searchitem-channel_inforow">
+        <source>%0 • %1</source>
+        <translation>%0 • %1</translation>
+    </message>
+    <message id="newpipe-searchitem-playlist_stream_count" numerus="yes">
+        <source>%n items</source>
+        <oldsource>%n item(s)</oldsource>
+        <translation>
+            <numerusform>%n oggetto</numerusform>
+            <numerusform>%n oggetti</numerusform>
+        </translation>
+    </message>
+    <message id="newpipe-searchitem-playlist_inforow">
+        <source>%0 • %1</source>
+        <translation>%0 • %1</translation>
+    </message>
+</context>
+</TS>
