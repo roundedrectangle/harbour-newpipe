6f70b6917ea6d8c010e3410c522689bb5d067516
David Llewellyn-Jones <david@flypig.co.uk>
2025-07-19 18:42:09 +0200

    Translated using Weblate (English)
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: harbour-newpipe/harbour-newpipe
    Translate-URL: https://hosted.weblate.org/projects/harbour-newpipe/harbour-newpipe/en/

--- sfos/harbour-newpipe/translations/harbour-newpipe-en.ts
+++ sfos/harbour-newpipe/translations/harbour-newpipe-en.ts
@@ -6,7 +6,7 @@
     <message id="newpipe-comment_item-replies" numerus="yes">
         <location filename="../qml/components/CommentItem.qml" line="116"/>
         <source>%n replies</source>
-        <translation type="unfinished">
+        <translation>
             <numerusform>%n reply</numerusform>
             <numerusform>%n replies</numerusform>
         </translation>
@@ -14,104 +14,104 @@
     <message id="newpipe-replies_page-header">
         <location filename="../qml/pages/RepliesPage.qml" line="43"/>
         <source>Replies</source>
-        <translation type="unfinished">Replies</translation>
+        <translation>Replies</translation>
     </message>
     <message id="newpipe-proglist_search-placeholder">
         <location filename="../qml/pages/SearchPage.qml" line="68"/>
         <source>Search</source>
-        <translation type="unfinished">Search</translation>
+        <translation>Search</translation>
     </message>
     <message id="newpipe-proglist_search-no-entries">
         <location filename="../qml/pages/SearchPage.qml" line="87"/>
         <source>No entries</source>
-        <translation type="unfinished">No entries</translation>
+        <translation>No entries</translation>
     </message>
     <message id="newpipe-proglist_search-enter-some-text">
         <location filename="../qml/pages/SearchPage.qml" line="89"/>
         <source>Enter some text to search</source>
-        <translation type="unfinished">Enter some text to search</translation>
+        <translation>Enter some text to search</translation>
     </message>
     <message id="newpipe-video_page-header">
         <location filename="../qml/pages/VideoPage.qml" line="41"/>
         <source>YouTube Video</source>
-        <translation type="unfinished">YouTube Video</translation>
+        <translation>YouTube Video</translation>
     </message>
     <message id="newpipe-cover-title">
         <location filename="../qml/cover/CoverPage.qml" line="9"/>
         <source>NewPipe</source>
-        <translation type="unfinished"></translation>
+        <translation>NewPipe</translation>
     </message>
     <message id="newpipe-media_details-detail_uploader">
         <location filename="../qml/components/MediaDetails.qml" line="63"/>
         <source>Uploader</source>
-        <translation type="unfinished"></translation>
+        <translation>Uploader</translation>
     </message>
     <message id="newpipe-media_details-detail_date">
         <location filename="../qml/components/MediaDetails.qml" line="69"/>
         <source>Upload date</source>
-        <translation type="unfinished"></translation>
+        <translation>Upload date</translation>
     </message>
     <message id="newpipe-media_details-detail_length">
         <location filename="../qml/components/MediaDetails.qml" line="75"/>
         <source>Length</source>
-        <translation type="unfinished"></translation>
+        <translation>Length</translation>
     </message>
     <message id="newpipe-media_details-detail_category">
         <location filename="../qml/components/MediaDetails.qml" line="81"/>
         <source>Category</source>
-        <translation type="unfinished"></translation>
+        <translation>Category</translation>
     </message>
     <message id="newpipe-media_details-detail_licence">
         <location filename="../qml/components/MediaDetails.qml" line="87"/>
         <source>Licence</source>
-        <translation type="unfinished"></translation>
+        <translation>Licence</translation>
     </message>
     <message id="newpipe-media_details-detail_description">
         <location filename="../qml/components/MediaDetails.qml" line="93"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Description</translation>
     </message>
     <message id="newpipe-searchitem-stream_inforow">
         <location filename="../src/searchitemstream.cpp" line="66"/>
         <source>%0 • %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%0 • %1</translation>
     </message>
     <message id="newpipe-searchitem-channel_stream_count" numerus="yes">
         <location filename="../src/searchitemchannel.cpp" line="74"/>
         <source>%n items</source>
         <oldsource>%n item(s)</oldsource>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n item</numerusform>
+            <numerusform>%n items</numerusform>
         </translation>
     </message>
     <message id="newpipe-searchitem-channel_subscriber_count" numerus="yes">
         <location filename="../src/searchitemchannel.cpp" line="77"/>
         <source>%n subscribers</source>
         <oldsource>%n subscriber(s)</oldsource>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n subscriber</numerusform>
+            <numerusform>%n subscribers</numerusform>
         </translation>
     </message>
     <message id="newpipe-searchitem-channel_inforow">
         <location filename="../src/searchitemchannel.cpp" line="80"/>
         <source>%0 • %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%0 • %1</translation>
     </message>
     <message id="newpipe-searchitem-playlist_stream_count" numerus="yes">
         <location filename="../src/searchitemplaylist.cpp" line="75"/>
         <source>%n items</source>
         <oldsource>%n item(s)</oldsource>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n item</numerusform>
+            <numerusform>%n items</numerusform>
         </translation>
     </message>
     <message id="newpipe-searchitem-playlist_inforow">
         <location filename="../src/searchitemplaylist.cpp" line="78"/>
         <source>%0 • %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%0 • %1</translation>
     </message>
 </context>
 </TS>
