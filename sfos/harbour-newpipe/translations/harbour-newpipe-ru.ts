701c126442cd1a01ac7ea84cefc31618d3eb403e
RoundedRectangle <129410215+roundedrectangle@users.noreply.github.com>
2025-08-01 21:37:44 +0200

    Added translation using Weblate (Russian)

---
+++ sfos/harbour-newpipe/translations/harbour-newpipe-ru.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name></name>
+    <message id="newpipe-comment_item-replies" numerus="yes">
+        <source>%n replies</source>
+        <translation type="unfinished">
+            <numerusform>%n reply</numerusform>
+            <numerusform>%n replies</numerusform>
+        </translation>
+    </message>
+    <message id="newpipe-replies_page-header">
+        <source>Replies</source>
+        <translation type="unfinished">Replies</translation>
+    </message>
+    <message id="newpipe-proglist_search-placeholder">
+        <source>Search</source>
+        <translation type="unfinished">Search</translation>
+    </message>
+    <message id="newpipe-proglist_search-no-entries">
+        <source>No entries</source>
+        <translation type="unfinished">No entries</translation>
+    </message>
+    <message id="newpipe-proglist_search-enter-some-text">
+        <source>Enter some text to search</source>
+        <translation type="unfinished">Enter some text to search</translation>
+    </message>
+    <message id="newpipe-video_page-header">
+        <source>YouTube Video</source>
+        <translation type="unfinished">YouTube Video</translation>
+    </message>
+    <message id="newpipe-cover-title">
+        <source>NewPipe</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="newpipe-media_details-detail_uploader">
+        <source>Uploader</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="newpipe-media_details-detail_category">
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="newpipe-media_details-detail_description">
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="newpipe-media_details-detail_date">
+        <source>Upload date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="newpipe-media_details-detail_length">
+        <source>Length</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="newpipe-media_details-detail_licence">
+        <source>Licence</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="newpipe-searchitem-stream_inforow">
+        <source>%0 • %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="newpipe-searchitem-channel_stream_count" numerus="yes">
+        <source>%n items</source>
+        <oldsource>%n item(s)</oldsource>
+        <translation type="unfinished">
+            <numerusform>%n item</numerusform>
+            <numerusform>%n items</numerusform>
+        </translation>
+    </message>
+    <message id="newpipe-searchitem-channel_subscriber_count" numerus="yes">
+        <source>%n subscribers</source>
+        <oldsource>%n subscriber(s)</oldsource>
+        <translation type="unfinished">
+            <numerusform>%n subscriber</numerusform>
+            <numerusform>%n subscribers</numerusform>
+        </translation>
+    </message>
+    <message id="newpipe-searchitem-channel_inforow">
+        <source>%0 • %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="newpipe-searchitem-playlist_stream_count" numerus="yes">
+        <source>%n items</source>
+        <oldsource>%n item(s)</oldsource>
+        <translation type="unfinished">
+            <numerusform>%n item</numerusform>
+            <numerusform>%n items</numerusform>
+        </translation>
+    </message>
+    <message id="newpipe-searchitem-playlist_inforow">
+        <source>%0 • %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
